21ebdd540cd83b1c7645fda451b8b54536edeb59
Kayra <KayraAlat@gmail.com>
2019-01-19 09:36:38 +0000

    Added snippet list and detail views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
