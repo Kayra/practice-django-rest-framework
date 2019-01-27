f777368e5f28a1ac72c9501655195137fa527155
Kayra <KayraAlat@gmail.com>
2019-01-27 19:32:21 +0000

    Added user association on snippet creation

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -6,6 +6,8 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
 
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'line_numbers', 'language', 'style',)
