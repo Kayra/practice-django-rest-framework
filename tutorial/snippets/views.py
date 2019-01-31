8baf6e6b1db2ed009c228e1abc87c788b21d3491
Kayra <KayraAlat@gmail.com>
2019-01-31 19:51:59 +0000

    Added custom obj owner permission to snippet detail

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
 
 
@@ -18,7 +19,8 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly)
 
 
 class UserList(generics.ListAPIView):
