eb6e9b305958e620f39c267a2e765e1a311e02ec
Kayra <KayraAlat@gmail.com>
2019-02-03 20:16:31 +0000

    Added hyperlinks to serialization

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,22 +1,23 @@
-from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework import serializers
 
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'line_numbers', 'language', 'style', 'owner',)
+        fields = ('url', 'id', 'highlight', 'title', 'code', 'line_numbers', 'language', 'style', 'owner',)
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
